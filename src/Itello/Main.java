ec64af128dcc666140efd8b19c6c481df576951d
the-yellow-kitty-cat <the.yellow.kitty.cat@gmail.com>
2016-11-10 15:09:01 +0100

    Created by COfver

--- src/Itello/Main.java
+++ src/Itello/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter n string");
+        System.out.println("Enter a string");
         String n = in.nextLine();
         if (n.equals("")) {
             System.out.println(n);
